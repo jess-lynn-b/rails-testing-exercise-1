968117e0279a5c4f15274ec058f44d43c31e3a60
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2024-02-08 22:35:34 -0600

    testing example 1

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  it 'should be invalid without a title' do
+    article = Article.create(title: nil, content: Faker::Lorem.paragraph_by_chars(number: 100))
+    expect(article).not_to be_valid
+  end
+
+    it 'should be invalid without content' do
+      article = Article.create(title: 'This is a title', content: nil)
+      expect(article).not_to be_valid
+    end
+
+    it 'should be invalid with a title less than 5 characters' do
+      article = Article.create(title: Faker::Lorem.characters(number: 4), content: Faker::Lorem.paragraph_by_chars(number: 100))
+      expect(article).not_to be_valid
+    end
+
+    it 'should be invalid with content less than 50 characters' do
+      article = Article.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph_by_chars(number: 25))
+      expect(article).not_to be_valid
+    end
+
+    it 'should be valid with title >= 5 characters and content >= 50 characters' do
+      article = Article.create(title: Faker::Lorem.characters(number: 5), content: Faker::Lorem.paragraph_by_chars(number: 50))
+      expect(article).to be_valid
+    end
+
+end
